c98a93d1744ce194995ff6155d38128b5b4483ad
Nina <ninamiladinovic@hotmail.com>
2017-12-16 14:39:12 +0100

    inicijalizacija projekta

---
+++ src/main/java/com/isa/ISA/IsaApplication.java
@@ -0,0 +1,12 @@
+package com.isa.ISA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaApplication.class, args);
+	}
+}
