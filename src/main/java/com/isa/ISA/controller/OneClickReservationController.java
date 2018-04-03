25d3f30df1ce906f77fae14b335d6b7008cc3d2a
Milan-Stankovic <milans995@gmail.com>
2018-04-03 16:09:59 +0200

    One click reservation dodat

---
+++ src/main/java/com/isa/ISA/controller/OneClickReservationController.java
@@ -0,0 +1,37 @@
+package com.isa.ISA.controller;
+
+import com.isa.ISA.dbModel.Rezervacija;
+import com.isa.ISA.service.OneClickReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class OneClickReservationController {
+
+    @Autowired
+    private OneClickReservationService ocrSevice;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/oneClick/{id}")
+    public void addOneClickReservation(@RequestBody Rezervacija r, @PathVariable Long id){
+        ocrSevice.setOneClick(r);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/oneClick/{id}")
+    public void updateClickReservation(@RequestBody Rezervacija r, @PathVariable Long id){
+        ocrSevice.updateOneClick(r);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/oneClick/{id}")
+    public void deleteOneClickReservation(@PathVariable Long id){
+        ocrSevice.deleteOneClick(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/oneClickReserve/{id}")
+    public void reserve(@RequestBody Rezervacija r, @PathVariable Long id){
+        ocrSevice.reserveSeat(r);
+    }
+}
--- src/main/java/com/isa/ISA/service/OneClickReservationService.java
+++ src/main/java/com/isa/ISA/service/OneClickReservationService.java
@@ -1,20 +1,57 @@
 package com.isa.ISA.service;
 
+
 import com.isa.ISA.dbModel.Rezervacija;
+
+import com.isa.ISA.dbModel.enums.Status;
+import com.isa.ISA.dbModel.korisnici.Poziv;
+import com.isa.ISA.repository.PozivRepository;
+import com.isa.ISA.repository.RezervacijaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 
-@Transactional(readOnly = true)
+@Service
 public class OneClickReservationService {
+
+    @Autowired
+    private RezervacijaRepository repo;
+
+    @Autowired
+    private PozivRepository pozRepo;
 
     @Transactional(
             propagation = Propagation.REQUIRES_NEW,
             readOnly = false)
-    public Rezervacija reserveSeat(Rezervacija r){
+    public Rezervacija reserveSeat(Rezervacija r){ // Treba testirati
 
-        return r;
+        Rezervacija r1 = repo.findOne(r.getId());
+        Poziv p = r1.getUrezervaciji().get(0);
+        if(r1.getRezervisao() == null) {
+            r1.setRezervisao(r.getRezervisao());
+
+            p.setStatus(Status.PRIHVACENO);
+            p.setOsoba(r.getRezervisao());
+            p.setPozvan(false);
+
+            pozRepo.save(p);
+            repo.save(r1);
+        }
+        return r1;
+    }
+
+    public void setOneClick(Rezervacija r){
+        if(repo.findOne(r.getId()).getRezervisao() == null)
+            repo.save(r);
+    }
+
+    public void deleteOneClick(Long r){
+        repo.delete(r);
+    }
+
+    public void updateOneClick(Rezervacija r){
+        repo.save(r);
     }
 }
