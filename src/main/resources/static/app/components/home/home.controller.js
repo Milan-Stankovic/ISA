4f2346f428576dee3ab841563da48e1f850d3ef2
Milan-Stankovic <milans995@gmail.com>
2018-04-10 16:56:09 +0200

    Home

--- src/main/resources/static/app/components/home/home.controller.js
+++ src/main/resources/static/app/components/home/home.controller.js
@@ -5,14 +5,51 @@
 		.module('app')
 		.controller('homeController', homeController);
 
-    homeController.$inject = ['$location', '$scope', '$rootScope'];
-    function homeController($location, $scope, $rootScope) {
+    homeController.$inject = ['$location', '$scope', '$rootScope','$http', '$cookies'];
+    function homeController($location, $scope, $rootScope, $http, $cookies) {
         var hc = this;
         hc.home = "Home";
         var init = function (){
         	//$location.path("/login");
         };
         init();
+
+
+        $scope.pozorista = {};
+        $scope.bioskopi = {};
+
+        var getPozorista = function () {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8096/p',
+            }).then(function successCallback(response) {
+                $scope.pozorista = response.data;
+                console.log(response.data);
+
+            }, function errorCallback(response) {
+                alert("Greska kod pozorista")
+
+            });
+        }
+
+        var getMovies = function () {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8096/b',
+            }).then(function successCallback(response) {
+                $scope.bioskopi = response.data;
+                console.log(response.data);
+
+            }, function errorCallback(response) {
+                alert("Greska kod bioskopa")
+
+            });
+        }
+
+        getMovies();
+        getPozorista();
+
+
     }
 
 
