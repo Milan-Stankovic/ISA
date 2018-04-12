9907de1171969b64967e4b1b3addf7970615bf58
tanjaindjic <mali.patuljko@gmail.com>
2018-04-12 23:53:07 +0200

    dodat dto i ispravljene neke greske

---
+++ src/main/java/com/isa/ISA/DTO/RegKorDTO.java
@@ -0,0 +1,42 @@
+package com.isa.ISA.DTO;
+
+public class RegKorDTO {
+
+    private String userName;
+    private String password;
+    private String ime;
+    private String prezime;
+    private String email;
+    private String grad;
+    private String brojTelefona;
+
+    public RegKorDTO(){}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public String getBrojTelefona() {
+        return brojTelefona;
+    }
+}
