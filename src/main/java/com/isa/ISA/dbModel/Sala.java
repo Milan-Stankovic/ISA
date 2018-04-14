d9613aad8b0ade4c780a713abf075386bf9bade3
Milan-Stankovic <milans995@gmail.com>
2018-04-14 14:34:57 +0200

    Jos malo sale ali dnd time

--- src/main/java/com/isa/ISA/dbModel/Sala.java
+++ src/main/java/com/isa/ISA/dbModel/Sala.java
@@ -2,11 +2,7 @@ package com.isa.ISA.dbModel;
 
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -26,7 +22,7 @@ public class Sala {
     /**
      * Lista svih sedisat u sali
      */
-    @OneToMany
+    @OneToMany(cascade = CascadeType.PERSIST)
     private List<Sediste> sedista;
 
     private String ime;
